db313d6dbdd0313370341a392d85e128b4926d86
= <=>
2019-08-27 02:11:48 +0700

    Update admin.php

--- admin.php
+++ admin.php
@@ -16,15 +16,3 @@ $qr = mysql_select_db("select * from admin") or die ("Error");
 		<th>Email</th>
 	</tr>
 </table>
-<?php 
-$i =1;
-while ($rs = mysql_fetch_array($qr)) {
-	echo "<tr>";
-	echo "<td>".$i."</td>";
-	echo "<td>".$rs["username"]."</td>";
-	echo "<td>***************</td>";
-	echo "<td>".$rs["email"]."</td>";
-	echo "</tr>";
-}
-}
-?>
