42d744f9ebf7543b60e5628cc3393d376a04ec93
= <=>
2019-08-27 01:37:36 +0700

    aaa

--- connect.php
+++ connect.php
@@ -1,6 +1,6 @@
 <?php
   if (empty(getenv("DATABASE_URL"))){
-      $pdo = new PDO('pgsql:host=localhost;port=5432;dbname=postgres', 'postgres', 'Candice');
+      $pdo = new PDO('pgsql:host=localhost;port=5432;dbname=d4bvd2cc5jlt96', 'iloapxkhzeuvec', '5f71779c31e9a25d696cfe511c87151a96a3bd72a3c35f79e559e8e10c4366f2');
   }  else {
     $db = parse_url(getenv("DATABASE_URL"));
     $pdo = new PDO("pgsql:" . sprintf(
