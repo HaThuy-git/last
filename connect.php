ddc6e932ce04866034366373d16070b3b7c3e163
= <=>
2019-08-26 23:08:08 +0700

    hi

---
+++ connect.php
@@ -0,0 +1,16 @@
+<?php
+  if (empty(getenv("DATABASE_URL"))){
+      $pdo = new PDO('pgsql:host=localhost;port=5432;dbname=postgres', 'postgres', 'Candice');
+  }  else {
+    $db = parse_url(getenv("DATABASE_URL"));
+    $pdo = new PDO("pgsql:" . sprintf(
+        "host=%s;port=%s;user=%s;password=%s;dbname=%s",
+        $db["host"],
+        $db["port"],
+        $db["user"],
+        $db["pass"],
+        ltrim($db["path"], "/")
+    ));
+  }
+?>
+© 2019 GitHub, Inc.
