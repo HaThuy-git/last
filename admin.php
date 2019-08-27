a3aaa5aa43f8c71a793d3b4f7ba2f5e237060c37
= <=>
2019-08-27 12:16:09 +0700

    b

--- admin.php
+++ admin.php
@@ -1,10 +1,28 @@
 <?php 
 require_once './connect.php';
-$servername = "localhost";
-$username = "username";
-$password = "password";
-
+  $sql = "SELECT adminusername, adminpassword FROM admin";
+  $stmt = $GLOBALS['pdo']->prepare($sql);
+  //Thiết lập kiểu dữ liệu trả về
+  $stmt->setFetchMode(PDO::FETCH_ASSOC);
+  $stmt->execute();
+  $resultSet = $stmt->fetchAll();
+  if (isset($_POST['submit1'])) {
+    $username = $_POST['username'];
+    $password = $_POST['password'];
+    foreach ($resultSet as $row) {
+      if ($username == $row['adminusername'] && $password ==  $row['adminpassword']){
+        header("Location: Staff/view-staff.php?username={$username}");
+      }else{
+        echo '
+          <script>
+            alert("WRONG! Wanna try again?");
+          </script>
+        ';
+      }
+    }
+  }
 ?>
+
 <h1>WELCOME ADMIN- USER LIST</h1>
 <table border="1">
 	<tr>
