b86eb1e1c872f686457acf4f3e69ac91299a879d
= <=>
2019-08-28 09:59:49 +0700

    Update admin.php

--- admin.php
+++ admin.php
@@ -1,9 +1,16 @@
+
 <?php
             require_once './database.php';
             $sql = "SELECT * FROM admin";
             $stmt = $pdo->prepare($sql);
             foreach ($pdo->query($sql) as $row) {
-?>
+            ?>
+                <tr>
+                    <td class="info"><?php echo $row['id']?></td> 
+                    <td class="info"><?php echo $row['username']?></td> 
+                    <td class="info"><?php echo $row['password']?></td> 
+                    <td class="info"><?php echo $row['email']?></td> 
+                   
 <h1>WELCOME ADMIN- USER LIST</h1>
 <table border="1">
     <tr>
