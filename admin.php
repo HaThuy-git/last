5fc99760cd3b4ef9303fd87b5fa61797e3ec6b2a
= <=>
2019-08-28 09:53:26 +0700

    aaa

--- admin.php
+++ admin.php
@@ -1,34 +1,16 @@
-<?php 
-require_once './connect.php';
-  $sql = "SELECT adminusername, adminpassword FROM admin";
-  $stmt = $GLOBALS['pdo']->prepare($sql);
-  //Thiết lập kiểu dữ liệu trả về
-  $stmt->setFetchMode(PDO::FETCH_ASSOC);
-  $stmt->execute();
-  $resultSet = $stmt->fetchAll();
-  if (isset($_POST['submit1'])) {
-    $username = $_POST['username'];
-    $password = $_POST['password'];
-    foreach ($resultSet as $row) {
-      if ($username == $row['adminusername'] && $password ==  $row['adminpassword']){
-        header("Location: Staff/view-staff.php?username={$username}");
-      }else{
-        echo '
-          <script>
-            alert("WRONG! Wanna try again?");
-          </script>
-        ';
-      }
-    }
-  }
+<?php
+            require_once './database.php';
+            $sql = "SELECT * FROM admin";
+            $stmt = $pdo->prepare($sql);
+            foreach ($pdo->query($sql) as $row) {
 ?>
-
 <h1>WELCOME ADMIN- USER LIST</h1>
 <table border="1">
-	<tr>
-		<th>ID</th>
-		<th>User name</th>
-		<th>Password</th>
-		<th>Email</th>
-	</tr>
+    <tr>
+        <th>ID</th>
+        <th>User name</th>
+        <th>Password</th>
+        <th>Email</th>
+    </tr>
 </table>
+                   
--- connect.php
+++ connect.php
@@ -1,15 +1,9 @@
+
 <?php
-  if (empty(getenv("DATABASE_URL"))){
-      $pdo = new PDO('pgsql:host=localhost;port=5432;dbname=d4bvd2cc5jlt96', 'iloapxkhzeuvec', '5f71779c31e9a25d696cfe511c87151a96a3bd72a3c35f79e559e8e10c4366f2');
-  }  else {
-    $db = parse_url(getenv("DATABASE_URL"));
-    $pdo = new PDO("pgsql:" . sprintf(
-        "host=%s;port=%s;user=%s;password=%s;dbname=%s",
-        $db["host"],
-        $db["port"],
-        $db["user"],
-        $db["pass"],
-        ltrim($db["path"], "/")
-    ));
-  }
-?>
+$connection = pg_connect("host=ec2-23-23-182-18.compute-1.amazonaws.com port=5432 
+    dbname=d4bvd2cc5jlt96 user=iloapxkhzeuvec 
+    password=5f71779c31e9a25d696cfe511c87151a96a3bd72a3c35f79e559e8e10c4366f2");  
+ if(!$connection) {
+     die("Database connection failed");
+ }
+ ?>
