29517547fe12d764a4c9b733c7b7ad55c73db211
= <=>
2019-08-28 19:19:26 +0700

    Update processlogin.php

--- processlogin.php
+++ processlogin.php
@@ -1,13 +1,23 @@
 <?php
     include 'connect.php';
-    $sql = "SELECT * FROM account";
+    $sql = "select * from account where username ='" .$user . "' password='" . $password . "' and email='" .$email . "'";
+
+$rows = query($sql);
+
+if(count($rows)>0)
+include("index.php");
+else
+    echo "<h1>Username or Password incorrect</h1>";
+}
+else
+    echo "<h1>Your account has no system</h1>";
     $result = pg_query($connection,$sql);
     if (pg_num_rows($result) > 0) {
     // output data of each row
     while($row = pg_fetch_assoc($result)) {
 
     ?>
-    <div action="index.php">
+    <div action="processlogin.php">
         <tr>
             <td>
                 <span><?php echo $row["username"]?></span>
