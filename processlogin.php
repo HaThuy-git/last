89ecc8071f540d3ba83343775085f073461d9a10
= <=>
2019-08-28 19:24:02 +0700

    Update processlogin.php

--- processlogin.php
+++ processlogin.php
@@ -2,8 +2,6 @@
     include 'connect.php';
     $sql = "select * from account where username ='" .$user . "' password='" . $password . "' and email='" .$email . "'";
 
-$rows = query($sql);
-
 if(count($rows)>0)
 include("index.php");
 else
@@ -14,27 +12,7 @@ else
     $result = pg_query($connection,$sql);
     if (pg_num_rows($result) > 0) {
     // output data of each row
-    while($row = pg_fetch_assoc($result)) {
-
-    ?>
-    <div action="processlogin.php">
-        <tr>
-            <td>
-                <span><?php echo $row["username"]?></span>
-            </td>
-
-            <td>
-                <span><?php echo $row["email"]?></span>
-            </td>
-
-             <td>
-                <span><?php echo $row["password"]?></span>
-            </td>
-            
-        <tr/>
-
-    </div>  
-    <?php }} 
+    while($row = pg_fetch_assoc($result)) 
 ?>
 
  
