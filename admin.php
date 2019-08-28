99947dc766bc307513641898e84814f0b663e481
= <=>
2019-08-28 10:21:08 +0700

    Update admin.php

--- admin.php
+++ admin.php
@@ -13,21 +13,7 @@
         <th>Password</th>
         <th>Email</th>
     </tr>
-    <?php
-            require_once 'connect.php';
-            $sql = "SELECT * FROM admin";
-            $stmt = $pdo->prepare($sql);
-            foreach ($pdo->pg_query($connection,$sql) as $row) {
-?>
-                <tr>
-                    <td class="info"><?php echo $row['id']?></td> 
-                    <td class="info"><?php echo $row['username']?></td> 
-                    <td class="info"><?php echo $row['password']?></td> 
-                    <td class="info"><?php echo $row['email']?></td> 
-                   
-                </tr>
-            }}
-            ?>
+    
 </table>
 </body>
 </html>
