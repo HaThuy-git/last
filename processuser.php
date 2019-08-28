2dc2c4b5a0afc44179db5ad264922497d2fcdde7
= <=>
2019-08-28 20:10:24 +0700

    Update processuser.php

--- processuser.php
+++ processuser.php
@@ -3,9 +3,9 @@ $u = $_POST['username'];
 $p = md5($_POST['password']); 
 $email = $_POST['email'];
 $sql = "SELECT * FROM admin";
-$result = pg_query("insert into admin(username, password, email) value ('$u', '$p', 'email') ") or die ("Lỗi truy vấn ") ;
+$result = pg_query("insert into admin(username, password, email) values ('$u', '$p', 'email') ") or die ("Lỗi truy vấn ") ;
 if($result) {
-	header("https://babyhop.herokuapp.com/processadd.php");
+	header("https://babyhop.herokuapp.com/admin.php");
 }
 
 ?>
