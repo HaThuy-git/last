67ebb8732361ed661aaae28d548e70ab7d8cbbd2
= <=>
2019-08-28 19:00:02 +0700

    Update processlogin.php

--- processlogin.php
+++ processlogin.php
@@ -3,11 +3,19 @@
     $sql = "SELECT * FROM account";
     $result = pg_query($connection,$sql);
     if (pg_num_rows($result) > 0) {
+    else
+    echo "<h1>Username or Password incorrect</h1>";
+}
+else
+    echo "<h1>Your account has no system</h1>";
+    
     // output data of each row
     while($row = pg_fetch_assoc($result)) {
-    
+
+include("index.php");
+
     ?>
-    <div action="processlogin.php">
+    <div action="processlogin.php.php">
         <tr>
             <td>
                 <span><?php echo $row["username"]?></span>
