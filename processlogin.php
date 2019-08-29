5be75582b9afb023a3b08e1f0ad4caf25f4a1dff
= <=>
2019-08-29 22:28:14 +0700

    s

--- processlogin.php
+++ processlogin.php
@@ -5,6 +5,26 @@
     if (pg_num_rows($result) > 0) {
     // output data of each row
     while($row = pg_fetch_assoc($result)) {
+
+    ?>
+    <div action="processlogin.php">
+        <tr>
+            <td>
+                <span><?php echo $row["username"]?></span>
+            </td>
+
+            <td>
+                <span><?php echo $row["email"]?></span>
+            </td>
+
+             <td>
+                <span><?php echo $row["password"]?></span>
+            </td>
+            
+        <tr/>
+
+    </div>  
+    <?php }} 
 ?>
 
  
