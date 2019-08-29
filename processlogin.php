c9710ab5491b21c313ab0849c534d991d1433549
= <=>
2019-08-29 23:23:02 +0700

    Update processlogin.php

--- processlogin.php
+++ processlogin.php
@@ -4,20 +4,23 @@ if(isset($_POST["username"]) && isset($_POST["password"]))
 {
     $user = $_POST["username"];
     $pass = $_POST["password"];
-    $sql ="SELECT * FROM account WHERE username = '$user' AND pwd= '$pass'";
+    $sql = "SELECT * FROM account WHERE username = '$username' AND pwd= '$password'";
     $rows = pg_query($sql);
-    if(pg_num_rows($rows)==1) { ?>
+    if(pg_num_rows($rows)==1) 
+    { ?>
         <script>
             alert("Congratulations Login Successfully!!");
         </script>
     <?php
-    } else { 
-        ?>
-            <script>
-                alert(" Username Or Password Is Wrong");
-                window.location.href = "/index.php";
-            </script>
-        <?php }
+    } 
+    else 
+    { 
+    ?>
+        <script>
+            alert(" Username Or Password Is Wrong");
+            window.location.href = "/index.php";
+        </script>
+    <?php }
 }
 ?>
 
