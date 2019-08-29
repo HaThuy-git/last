cdce9c370acd8242192a53f99773e95ef9e2f520
= <=>
2019-08-29 21:25:56 +0700

    Update add_user.php

--- add_user.php
+++ add_user.php
@@ -1,3 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	
+	<title>Add usert</title>
+</head>
+<body>
+	<?php 
+require 'connect.php'; 
+
 <h1>ADD USER </h1>
 <form action="processadd.php" method="post">
 	User name: <input type="text" name="user"> <br> <br>
@@ -5,4 +15,6 @@
 	Email: <input type="password" name="pass"><br> <br>
 	<input type="Submit" value="Add User">
 </form>
-
+?>
+</body>
+</html>
