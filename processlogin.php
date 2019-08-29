f6ec28303945ec0d4b9e4dbab337e8c7d6df0552
= <=>
2019-08-30 00:08:12 +0700

    Update processlogin.php

--- processlogin.php
+++ processlogin.php
@@ -7,8 +7,9 @@ if(isset($_POST["username"]) && isset($_POST["password"]))
     $user = $_POST["username"];
     $pass = $_POST["password"];
     $sql = "SELECT * FROM account WHERE username = '$username' AND pwd= '$password'";
-    $rows = pg_query($sql);
-    if(pg_num_rows($rows)==1) 
+  
+    $result = pg_query($connection,$sql);
+    if(pg_num_rows($result)==1) 
     { ?>
         <script>
             alert("Congratulations Login Successfully!!");
