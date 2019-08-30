65958e1f12db3d1e8233e3fa8f7450351e98832e
= <=>
2019-08-30 09:40:32 +0700

    Update processlogin.php

--- processlogin.php
+++ processlogin.php
@@ -9,12 +9,8 @@ if(isset($_POST["username"]) && isset($_POST["password"]))
   
     $result = pg_query($connection,$sql);
     if(pg_num_rows($result)==1) 
-    { ?>
-        <script>
-            alert("Congratulations Login Successfully!!");
-            window.location.href = "/index.php";
-        </script>
-    <?php
+    { 
+        header("Location: ./index.php");
     } 
     else 
     { 
